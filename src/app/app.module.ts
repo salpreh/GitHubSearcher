4eeeb66f1e0061e1b53da21d45151955aa5b9e34
salpreh <salva.perez46@gmail.com>
2018-10-08 20:24:14 +0200

    Added routing logic to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GitSearchService } from './git-search.service';
 
 import { AppComponent } from './app.component';
 import { GitSearchComponent } from './git-search/git-search.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
+const appRoutes: Routes = [
+  { path: '',
+    component: HomePageComponent
+  },
+  { path: 'search',
+    redirectTo: 'search/angular',
+    pathMatch: 'full'
+  },
+  { path: 'search/:query',
+    component: GitSearchComponent,
+    data: {
+      title: 'Git Search'
+    }
+  },
+  { path: '**',
+    component: NotFoundComponent
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
-    GitSearchComponent
+    GitSearchComponent,
+    HomePageComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [GitSearchService],
   bootstrap: [AppComponent]
